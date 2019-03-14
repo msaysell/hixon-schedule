b81b0d65de553098c4a3e0f631b9303243f39ea8
Mike Saysell <mike.saysell@buto.tv>
2019-03-14 23:32:51 +0000

    Initial regular events

--- Hixon/urls.py
+++ Hixon/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView, FormView
 from schedule import urls as schedule_urls
 from schedule.views import FullCalendarView
+from HixonCalendar.views import RegularEventsView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('calendar/', FullCalendarView.as_view(template_name='calendar.html'), name='calendar', kwargs={'calendar_slug': 'hixon'}),
     path('committee/', TemplateView.as_view(template_name='committee.html'), name='committee'),
     path('100-club/', TemplateView.as_view(template_name='hundred_club.html'), name='hundred_club'),
-    path('regular-events/', TemplateView.as_view(template_name='regular_events.html'), name='regular_events'),
+    path('regular-events/', RegularEventsView.as_view(), name='regular_events'),
     path('schedule/', include(schedule_urls)),
     path('admin/', admin.site.urls)
 ]
--- HixonCalendar/views.py
+++ HixonCalendar/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from calendar import day_name
+from django.views.generic.base import TemplateView
+from django.db.models.functions import TruncDay
+from schedule.models import Event
+from collections import defaultdict
+import operator
 
-# Create your views here.
+class RegularEventsView(TemplateView):
+
+    template_name = "regular_events.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        events = Event.objects.filter(rule__isnull=False).distinct('title')
+        grouped_events = defaultdict(list)
+        [grouped_events[e.start.weekday()].append(e) for e in events]
+        context['events'] = [(day_name[k], sorted(v, key=lambda e: e.start.hour)) for k, v in sorted(grouped_events.items(), key=operator.itemgetter(0))]
+        return context
